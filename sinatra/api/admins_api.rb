5174247fe803bd90564a9a2314622fe6a7deea34
malt-man <swaggerhubtest+1@gmail.com>
2019-05-13 12:58:16 +0000

    Generated code by SwaggerHub

--- sinatra/api/admins_api.rb
+++ sinatra/api/admins_api.rb
@@ -22,3 +22,25 @@ MyApp.add_route('POST', '/malt-v5-team/most-plugins/1.0.0/inventory', {
   {"message" => "yes, it worked"}.to_json
 end
 
+
+MyApp.add_route('DELETE', '/malt-v5-team/most-plugins/1.0.0/inventory', {
+  "resourcePath" => "/Admins",
+  "summary" => "removes an inventory item",
+  "nickname" => "removes_inventory", 
+  "responseClass" => "void", 
+  "endpoint" => "/inventory", 
+  "notes" => "Adds an item to the system",
+  "parameters" => [
+    {
+      "name" => "body",
+      "description" => "Inventory item to add",
+      "dataType" => "InventoryItem",
+      "paramType" => "body",
+    }
+    ]}) do
+  cross_origin
+  # the guts live here
+
+  {"message" => "yes, it worked"}.to_json
+end
+
